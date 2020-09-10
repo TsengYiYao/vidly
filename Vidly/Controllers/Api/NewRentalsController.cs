fb8748fd09f186a781af64055b17b5916569c583
TsengYiYao <yiyatsen@gmail.com>
2020-09-10 13:40:41 +0800

    10.6. Edge Cases

--- Vidly/Controllers/Api/NewRentalsController.cs
+++ Vidly/Controllers/Api/NewRentalsController.cs
@@ -19,11 +19,19 @@ namespace Vidly.Controllers.Api
             var customer = _context.Customers.Single(
                 c => c.Id == newRental.CustomerId);
 
-            var movies = _context.Movies.Where(
-                m => newRental.MovieIds.Contains(m.Id));
+            if (customer == null)
+                return BadRequest("CustomerId is not valid.");
 
+            var movies = _context.Movies.Where(
+                m => newRental.MovieIds.Contains(m.Id)).ToList();
+            
             foreach(var movie in movies)
             {
+                if (movie.NumberAvailable == 0)
+                    return BadRequest("Movie is not available.");
+
+                movie.NumberAvailable--;
+
                 var rental = new Rental
                 {
                     Customer = customer,
