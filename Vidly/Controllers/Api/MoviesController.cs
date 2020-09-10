2b858df2bdc394ccb6ff074f5b8dd8ee828dc01e
TsengYiYao <yiyatsen@gmail.com>
2020-09-10 15:48:20 +0800

    fix controllers

--- Vidly/Controllers/Api/MoviesController.cs
+++ Vidly/Controllers/Api/MoviesController.cs
@@ -18,16 +18,28 @@ namespace Vidly.Controllers.Api
             _context = new ApplicationDbContext();
         }
 
-        public IEnumerable<MovieDto> GetMovies()
+        //public IEnumerable<MovieDto> GetMovies()
+        //{
+        //    var moviesDto = _context.Movies
+        //        .Include(m => m.Genre)
+        //        .ToList()
+        //        .Select(Mapper.Map<Movie, MovieDto>);
+
+        //    return moviesDto;
+        //}
+        public IEnumerable<MovieDto> GetMovies(string query = null)
         {
-            var moviesDto = _context.Movies
+            var moviesQuery = _context.Movies
                 .Include(m => m.Genre)
+                .Where(m => m.NumberAvailable > 0);
+
+            if (!String.IsNullOrWhiteSpace(query))
+                moviesQuery = moviesQuery.Where(m => m.Name.Contains(query));
+
+            return moviesQuery
                 .ToList()
                 .Select(Mapper.Map<Movie, MovieDto>);
-
-            return moviesDto;
         }
-
         public IHttpActionResult GetMovie(int id)
         {
             var movie = _context.Movies.SingleOrDefault(c => c.Id == id);
